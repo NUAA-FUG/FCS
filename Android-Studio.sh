501df4cef80388353610e37e4182f30e69b62eb9
Triple-R <hyckcs@gmail.com>
2018-03-15 23:00:53 +0800

    add new file

---
+++ Android-Studio.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo yum install zlib.i686 ncurses-libs.i686 bzip2-libs.i686 -y
+
+# Open Downloads directory
+cd ~/Downloads
+wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.0.1.0/android-studio-ide-171.4443003-linux.zip
+
+# Unzip the package to /opt/ directory
+sudo unzip android-studio-ide-171.4443003-linux.zip -d /opt/
+
+cd /opt/android-studio/bin/
+./studio.sh 
