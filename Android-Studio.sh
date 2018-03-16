c42c7222a62369790088b9d58ef04cc312fb149d
Triple-R <hyckcs@gmail.com>
2018-03-16 17:31:23 +0800

    update

--- Android-Studio.sh
+++ Android-Studio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo yum install zlib.i686 ncurses-libs.i686 bzip2-libs.i686 -y
+sudo dnf install zlib.i686 ncurses-libs.i686 bzip2-libs.i686 -y
 
 # Open Downloads directory
 cd ~/Downloads
@@ -8,6 +8,8 @@ wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.0.1.0
 
 # Unzip the package to /opt/ directory
 sudo unzip android-studio-ide-171.4443003-linux.zip -d /opt/
+
+sudo rm android-studio-ide-171.4443003-linux.zip 
 
 cd /opt/android-studio/bin/
 ./studio.sh 
--- Listen1.sh
+++ Listen1.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-cd ~/Downloads
+cd /opt/
 
 # Download listen 1
-wget -q --show-progress https://github.com/listen1/listen1_desktop/releases/download/v1.5.2/Listen1_1.5.2_linuxx86_64.AppImage
+sudo wget -q --show-progress https://github.com/listen1/listen1_desktop/releases/download/v1.5.2/Listen1_1.5.2_linuxx86_64.AppImage
 
-chmod a+x Listen1_1.5.2_linuxx86_64.AppImage
+sudo chmod a+x Listen1_1.5.2_linuxx86_64.AppImage
 
-sudo ./Listen1_1.5.2_linuxx86_64.AppImage
+sudo mv Listen1_1.5.2_linuxx86_64.AppImage listen1.AppImage
 
-sudo rm Listen1_1.5.2_linuxx86_64.AppImage
+#./Listen1_1.5.2_linuxx86_64.AppImage
+./listen1.AppImage 
