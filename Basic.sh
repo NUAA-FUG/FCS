39966bdd57da7e6e2c2b346229f524d421d59206
Triple-R <hyckcs@gmail.com>
2018-03-24 14:50:57 +0800

    Update
    not complete wait test

--- Basic.sh
+++ Basic.sh
@@ -12,7 +12,37 @@ sudo dnf install -y vim git gcc-c++ gdb curl
 # Install NetworkManager plugins
 sudo dnf install NetworkManager-*
 
-# Some tools to support NTFS and exFAT filessysystem
+# Some tools to support NTFS and exFAT filesystem
 sudo dnf install -y ntfs-3g fuse fuse-libs
 sudo dnf install -y fuse-exfat
 
+######  Software ######
+
+# Install Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install -y code
+
+# Install Sublime Text 3
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+sudo dnf install -y sublime-text
+
+# Install Zeal
+sudo dnf install -y zeal
+
+# Install OBS
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install obs-studio
+
+# Install Calibre
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+
+# Install uget
+sudo dnf install uget
+
+# Install Google Chrome stable version
+sudo sh -c 'echo -e "[google-chrome]\nname=google-chrome\nbaseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64 \nenabled=1\ngpgcheck=1\ngpgkey=https://dl.google.com/linux/linux_signing_key.pub
+" > /etc/yum.repos.d/google-chrome.repo'
+sudo dnf check-update
