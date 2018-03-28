d7216889918aea059d9e6715e20811814eabf3dc
Triple-R <hyckcs@gmail.com>
2018-03-28 16:42:40 +0800

    Update

--- test.sh
+++ test.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-echo "Are you live in China?(y/n, Default:n): "
+echo -n "Are you live in China?(y/n, Default:n): ";
 read choice
-if [ $choice = "y" -o $choice = "Y"];
-then 
-    wget -q --show-progress https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm
-    sudo dnf install -y nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm
-    sudo proxychains4 dropbox start -i
+if [ $choice = "y" -o $choice = "Y" ];
+then
+    cd ~/Downloads
+    sudo dnf install python-gpgme -y
+    proxychains4 wget -q --show-progress https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm -O dropbox.rpm
+    sudo dnf install -y dropbox.rpm
+    proxychains4 dropbox start -i
     dropbox start
     dropbox proxy manual socks5 127.0.0.1 1080
 else
-    wget -q --show-progress https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm
-    sudo dnf install -y nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm
-    sudo dropbox start -i
+    cd ~/Downloads
+    sudo dnf install python-gpgme -y
+    wget -q --show-progress https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm -O dropbox.rpm
+    sudo dnf install -y dropbox.rpm
+    dropbox start -i
+	dropbox start
 fi
