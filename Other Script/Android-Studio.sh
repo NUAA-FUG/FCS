35e6f138bca3e0d04668ae8dcb4fab0b8aeca7eb
Triple-R <hyckcs@gmail.com>
2018-04-04 17:13:34 +0800

    Update
    Update android studio to 3.1 version

--- Other Script/Android-Studio.sh
+++ Other Script/Android-Studio.sh
@@ -4,12 +4,12 @@ sudo dnf install zlib.i686 ncurses-libs.i686 bzip2-libs.i686 -y
 
 # Open Downloads directory
 cd ~/Downloads
-wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.0.1.0/android-studio-ide-171.4443003-linux.zip
+wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.1.0.16/android-studio-ide-173.4670197-linux.zip
 
 # Unzip the package to /opt/ directory
-sudo unzip android-studio-ide-171.4443003-linux.zip -d /opt/
+sudo unzip android-studio-ide-173.4670197-linux.zip -d /opt/
 
-sudo rm android-studio-ide-171.4443003-linux.zip 
+sudo rm android-studio-ide-173.4670197-linux.zip 
 
 cd /opt/android-studio/bin/
 ./studio.sh 
