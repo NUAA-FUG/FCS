d220c541b0dd01d4277ee44badc7450c86940dac
Triple-R <hyckcs@gmail.com>
2018-04-06 13:20:15 +0800

    update

--- Other Script/IDEA-Community.sh
+++ Other Script/IDEA-Community.sh
@@ -2,11 +2,11 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/idea/ideaIC-2017.3.5.tar.gz
+wget -q --show-progress https://download.jetbrains.com/idea/ideaIC-2018.1.tar.gz
 
 # Unzip it to /opt/ directory
-sudo tar -zxvf ideaIC-2017.3.5.tar.gz -C /opt/
+sudo tar -zxvf ideaIC-2018.1.tar.gz -C /opt/
 
 # Install
-cd /opt/idea-IC-173.4674.33/bin/
+cd /opt/idea-IC-181.4203.550/bin/
 ./idea.sh 
