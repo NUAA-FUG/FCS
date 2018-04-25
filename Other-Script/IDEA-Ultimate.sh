dcb83c1e65d6ac758430501af862a0632763707f
Triple-R <hyckcs@gmail.com>
2018-04-25 15:19:39 +0800

    Upgrade IDEA :arrow_up:
    Version 2018.1 --> Version 2018.1.2

--- Other-Script/IDEA-Ultimate.sh
+++ Other-Script/IDEA-Ultimate.sh
@@ -2,14 +2,14 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/idea/ideaIU-2018.1.tar.gz
+wget -q --show-progress https://download.jetbrains.com/idea/ideaIU-2018.1.2.tar.gz
 
 # Unzip it to /opt/ directory
-sudo tar -zxvf ideaIU-2018.1.tar.gz -C /opt/
+sudo tar -zxvf ideaIU-2018.1.2.tar.gz -C /opt/
 
 # Delete packages
-sudo rm ideaIU-2018.1.tar.gz 
+sudo rm ideaIU-2018.1.2.tar.gz 
 
 # Install
-cd /opt/idea-IU-181.4203.550/bin/
+cd /opt/idea-IU-181.4668.68/bin/
 ./idea.sh 
