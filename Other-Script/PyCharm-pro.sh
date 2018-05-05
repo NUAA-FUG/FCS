1cd7e91bd0b71cbe9729ddd4e490b3f8eb2709f7
Triple-R <hyckcs@gmail.com>
2018-05-05 14:09:37 +0800

    Upgrade: Version 2018.1.2

--- Other-Script/PyCharm-pro.sh
+++ Other-Script/PyCharm-pro.sh
@@ -2,13 +2,13 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/python/pycharm-professional-2018.1.tar.gz
+wget -q --show-progress https://download.jetbrains.com/python/pycharm-professional-2018.1.2.tar.gz
 
 # Unzip tar.gz to /opt/ directory
-sudo tar -zxvf pycharm-professional-2018.1.tar.gz -C /opt/ --transform s/pycharm-2018.1/pycharm/
+sudo tar -zxvf pycharm-professional-2018.1.2.tar.gz -C /opt/ --transform s/pycharm-2018.1.2/pycharm/
 
 # Delete the packages
-sudo rm pycharm-professional-2018.1.tar.gz 
+sudo rm pycharm-professional-2018.1.2.tar.gz 
 
 cd /opt/pycharm/bin/
 
