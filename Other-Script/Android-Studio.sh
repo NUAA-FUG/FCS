203f7d2f03776209c8d583f5867b2af173b2b7bf
Triple-R <hyckcs@gmail.com>
2018-05-14 17:06:04 +0800

    Upgrade to version 3.1.2

--- Other-Script/Android-Studio.sh
+++ Other-Script/Android-Studio.sh
@@ -8,12 +8,12 @@ sudo dnf -y install qemu-img libvirt libvirt-python libvirt-client
 
 # Open Downloads directory
 cd ~/Downloads
-wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.1.0.16/android-studio-ide-173.4670197-linux.zip
+wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-linux.zip
 
 # Unzip the package to /opt/ directory
-sudo unzip android-studio-ide-173.4670197-linux.zip -d /opt/
+sudo unzip android-studio-ide-173.4720617-linux.zip -d /opt/
 
-sudo rm android-studio-ide-173.4670197-linux.zip 
+sudo rm android-studio-ide-173.4720617-linux.zip 
 
 cd /opt/android-studio/bin/
 ./studio.sh 
