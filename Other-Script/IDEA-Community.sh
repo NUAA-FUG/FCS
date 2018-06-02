25ccbb00e1d09547df0390aa2182d83f39691448
Triple-R <hyckcs@gmail.com>
2018-06-02 14:59:47 +0800

    :arrow_up:Upgrade IDEA and pycharm
    version 2018.1.3  ---> 2018.1.4

--- Other-Script/IDEA-Community.sh
+++ Other-Script/IDEA-Community.sh
@@ -2,13 +2,13 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/idea/ideaIC-2018.1.3.tar.gz
+wget -q --show-progress https://download.jetbrains.com/idea/ideaIC-2018.1.4.tar.gz
 
 # Unzip it to /opt/ directory
-sudo tar -zxvf ideaIC-2018.1.3.tar.gz -C /opt/ --transform s/idea-IC-181.4892.42/ideaIC/
+sudo tar -zxvf ideaIC-2018.1.4.tar.gz -C /opt/ --transform s/idea-IC-181.5087.20/ideaIC/
 
 # Delete tar package
-sudo rm ideaIC-2018.1.3.tar.gz
+sudo rm ideaIC-2018.1.4.tar.gz
 
 # Install
 cd /opt/ideaIC/bin/
--- Other-Script/IDEA-Ultimate.sh
+++ Other-Script/IDEA-Ultimate.sh
@@ -2,13 +2,13 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/idea/ideaIU-2018.1.3.tar.gz
+wget -q --show-progress https://download.jetbrains.com/idea/ideaIU-2018.1.4.tar.gz
 
 # Unzip it to /opt/ directory
-sudo tar -zxvf ideaIU-2018.1.3.tar.gz -C /opt/ --transform s/idea-IU-181.4892.42/ideaIU/
+sudo tar -zxvf ideaIU-2018.1.4.tar.gz -C /opt/ --transform s/idea-IU-181.5087.20/ideaIU/
 
 # Delete packages
-sudo rm ideaIU-2018.1.3.tar.gz 
+sudo rm ideaIU-2018.1.4.tar.gz 
 
 # Install
 cd /opt/ideaIU/bin/
