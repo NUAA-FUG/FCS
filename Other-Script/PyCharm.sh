b4d8d93c2ca9afdd32ba18bfe441153062ff949f
Triple-R <hyckcs@gmail.com>
2018-07-26 22:52:01 +0800

    add new things and upgrade to version 2018.2
    now you can choose pycharm professional version or community version to install.

--- Other-Script/PyCharm.sh
+++ Other-Script/PyCharm.sh
@@ -2,14 +2,19 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/python/pycharm-professional-2018.1.4.tar.gz
-
-# Unzip tar.gz to /opt/ directory
-sudo tar -zxvf pycharm-professional-2018.1.4.tar.gz -C /opt/ --transform s/pycharm-2018.1.4/pycharm/
-
-# Delete the packages
-sudo rm pycharm-professional-2018.1.4.tar.gz 
-
-cd /opt/pycharm/bin/
-
-./pycharm.sh
+echo -n "Which version do you want to install,Professional or Community?[P or C (default C)]: ";
+read pyv
+if [ $pyv = "p" -o $pyv = "P" ];
+then
+    wget -q --show-progress https://download.jetbrains.com/python/pycharm-professional-2018.2.tar.gz
+    sudo tar -zxvf pycharm-professional-2018.2.tar.gz -C /opt/ --transform s/pycharm-2018.2/pycharm/
+    sudo rm pycharm-professional-2018.2.tar.gz 
+    cd /opt/pycharm/bin/
+    ./pycharm.sh
+else
+    wget -q --show-progress https://download.jetbrains.com/python/pycharm-community-2018.2.tar.gz
+    sudo tar -zxvf pycharm-community-2018.2.tar.gz -C /opt/ --transform s/pycharm-community-2018.2/pycharm/
+    sudo rm pycharm-community-2018.2.tar.gz 
+    cd /opt/pycharm/bin/
+    ./pycharm.sh
+fi
