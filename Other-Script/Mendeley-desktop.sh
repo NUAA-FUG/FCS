b820b5af191e9e8f031408576d21c7c8264e025a
Triple-R <hyckcs@gmail.com>
2018-07-27 16:11:17 +0800

    rewrite and upgrade
    version 1.18  ----> version 1.19.1

--- Other-Script/Mendeley-desktop.sh
+++ Other-Script/Mendeley-desktop.sh
@@ -7,36 +7,13 @@ wget -q --show-progress https://www.mendeley.com/autoupdates/installer/Linux-x64
 
 # Unzip tar.bz2 to folder /opt/
 
-sudo tar -xvf mendeley.tar.bz2 -C /opt/
+sudo tar -xvf mendeley.tar.bz2 -C /opt/ --transform s/mendeleydesktop-1.19.1-linux-x86_64/mendeleydesktop/
 
-# Open /opt/ folder and rename mendeley
-cd /opt/
+# Delete package
+sudo rm mendeley.tar.bz2
 
-ls
-
-# if the folder named mendeleydesktop is already here.Delete it and start install.
-echo -n "Is there a folder named mendeleydesktop in /opt/ directory? [y/n (default is n]: ";
-read ans
-if [ $ans = "y" -o $ans = "Y" ];
-then
-    sudo rm -rf mendeleydesktop
-    sudo mv mendeleydesktop-1.18-linux-x86_64/ mendeleydesktop/
-    cd mendeleydesktop/bin/
-    ./install-mendeley-link-handler.sh /opt/mendeleydesktop/bin/mendeleydesktop 
-
-    sudo cp /opt/mendeleydesktop/share/icons/hicolor/128x128/apps/mendeleydesktop.png /usr/share/icons/
-else
-    sudo mv mendeleydesktop-1.18-linux-x86_64/ mendeleydesktop/
-    cd mendeleydesktop/bin/
-    ./install-mendeley-link-handler.sh /opt/mendeleydesktop/bin/mendeleydesktop 
-    sudo cp /opt/mendeleydesktop/share/icons/hicolor/128x128/apps/mendeleydesktop.png /usr/share/icons/
-fi
-
-# # Open /bin/ folder
-# cd mendeleydesktop/bin/
-
-# # Run install script
-# ./install-mendeley-link-handler.sh /opt/mendeleydesktop/bin/mendeleydesktop 
-
-# sudo cp /opt/mendeleydesktop/share/icons/hicolor/128x128/apps/mendeleydesktop.png /usr/share/icons/
+# Install
+cd /opt/mendeleydesktop/bin/
+./install-mendeley-link-handler.sh /opt/mendeleydesktop/bin/mendeleydesktop
+sudo cp /opt/mendeleydesktop/share/icons/hicolor/128x128/apps/mendeleydesktop.png /usr/share/icons/
 
