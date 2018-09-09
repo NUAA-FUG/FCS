55e7512443579ab0bcb0f9229b9cc16fcd414d91
Triple-R <hyckcs@gmail.com>
2018-09-09 23:39:17 +0800

    feat:Upgrade Mendeley
    version 1.19.1  ---> 1.19.2

--- Other-Script/Mendeley-desktop.sh
+++ Other-Script/Mendeley-desktop.sh
@@ -7,7 +7,7 @@ wget -q --show-progress https://www.mendeley.com/autoupdates/installer/Linux-x64
 
 # Unzip tar.bz2 to folder /opt/
 
-sudo tar -xvf mendeley.tar.bz2 -C /opt/ --transform s/mendeleydesktop-1.19.1-linux-x86_64/mendeleydesktop/
+sudo tar -xvf mendeley.tar.bz2 -C /opt/ --transform s/mendeleydesktop-1.19.2-linux-x86_64/mendeleydesktop/
 
 # Delete package
 sudo rm mendeley.tar.bz2
