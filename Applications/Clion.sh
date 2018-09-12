8786ddae452b3c7b28b2c31f966ec600ec62430b
Triple-R <hyckcs@gmail.com>
2018-09-12 11:23:45 +0800

    feat:upgrade Clion IDEA Pycharm Webstorm
    version 2018.2 ---> 2018.2.3

--- Applications/Clion.sh
+++ Applications/Clion.sh
@@ -6,7 +6,7 @@ cd ~/Downloads
 wget -q --show-progress https://data.services.jetbrains.com/products/download\?code\=CL\&platform\=linux -O clion.tar.gz
 
 # Unzip tar.gz to /opt/ directory
-sudo tar -zxvf clion.tar.gz -C /opt/ --transform s/clion-2018.2/clion/
+sudo tar -zxvf clion.tar.gz -C /opt/ --transform s/clion-2018.2.3/clion/
 
 # Delete package
 sudo rm clion.tar.gz 
--- Applications/WebStorm.sh
+++ Applications/WebStorm.sh
@@ -2,13 +2,13 @@
 
 cd ~/Downloads
 
-wget -q --show-progress https://download.jetbrains.com/webstorm/WebStorm-2018.2.tar.gz
+wget -q --show-progress https://download.jetbrains.com/webstorm/WebStorm-2018.2.3.tar.gz
 
 # Unzip tar.gz 
-sudo tar -zxvf WebStorm-2018.2.tar.gz -C /opt/ --transform s/WebStorm-182.3684.70/WebStorm/
+sudo tar -zxvf WebStorm-2018.2.3.tar.gz -C /opt/ --transform s/WebStorm-182.4323.44/WebStorm/
 
 # Delete package
-sudo rm WebStorm-2018.2.tar.gz
+sudo rm WebStorm-2018.2.3.tar.gz
 
 # Install
 cd /opt/WebStorm/bin/
