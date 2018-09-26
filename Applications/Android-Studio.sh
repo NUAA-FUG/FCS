230fa601f5f8384e91f4c2362d0a40dbff7cf621
Triple-R <hyckcs@gmail.com>
2018-09-26 12:28:22 +0800

    feat:upgrade Android Studio
    upgrade to version 3.2

--- Applications/Android-Studio.sh
+++ Applications/Android-Studio.sh
@@ -8,12 +8,12 @@ sudo dnf -y install qemu-img libvirt libvirt-python libvirt-client
 
 # Open Downloads directory
 cd ~/Downloads
-wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-linux.zip
+wget -q --show-progress https://dl.google.com/dl/android/studio/ide-zips/3.2.0.26/android-studio-ide-181.5014246-linux.zip
 
 # Unzip the package to /opt/ directory
-sudo unzip android-studio-ide-173.4720617-linux.zip -d /opt/
+sudo unzip android-studio-ide-181.5014246-linux.zip -d /opt/
 
-sudo rm android-studio-ide-173.4720617-linux.zip 
+sudo rm android-studio-ide-181.5014246-linux.zip
 
 cd /opt/android-studio/bin/
 ./studio.sh 
