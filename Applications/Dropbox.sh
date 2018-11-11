a79aacece721df7a7e0ad295bd0e87c0b897d8f0
Triple-R <hyckcs@gmail.com>
2018-11-11 17:30:32 +0800

    fix:replace python-gpgme with python2-gpg
    python-gpgme has been deprecated

--- Applications/Dropbox.sh
+++ Applications/Dropbox.sh
@@ -5,7 +5,7 @@ read choice
 if [ $choice = "y" -o $choice = "Y" ];
 then
     cd ~/Downloads
-    sudo dnf install python-gpgme -y
+    sudo dnf install python2-gpg -y
     proxychains4 wget -q --show-progress https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm -O dropbox.rpm
     sudo dnf install -y dropbox.rpm
     proxychains4 dropbox start -i
@@ -13,7 +13,7 @@ then
     dropbox proxy manual socks5 127.0.0.1 1080
 else
     cd ~/Downloads
-    sudo dnf install python-gpgme -y
+    sudo dnf install python2-gpg -y
     wget -q --show-progress https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm -O dropbox.rpm
     sudo dnf install -y dropbox.rpm
     dropbox start -i
